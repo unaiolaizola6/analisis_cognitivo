7fa6679e77ff4451350785301b4f863bb3602205
unaiolaizola6 <113601251+unaiolaizola6@users.noreply.github.com>
2023-05-12 09:05:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import requests
 
 resp = requests.get('http://analisis-metacognitivo2.aegcloud.pro')
 
-st.write(resp)
+st.write(resp.cookies)
 
 #LEER Y CLASIFICAR LAS RESPUESTAS
 data = pd.read_csv(r'objeto_si.csv')
