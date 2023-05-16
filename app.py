d3751ac6c383d137ed753cf78893f2fdff9fae5f
unaiolaizola6 <113601251+unaiolaizola6@users.noreply.github.com>
2023-05-16 10:17:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def result():
     return 'Received !' # response to your request.
 
 if __name__=='__main__':
-   app.run(debug=True, host='0.0.0.0', port=8080)
+   app.run(debug=True, port=8083)
 
 st.write("prueba")
     
