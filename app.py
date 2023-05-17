f34b8a059a50bd3997ad2f566c148fa0e620bbeb
unaiolaizola6 <113601251+unaiolaizola6@users.noreply.github.com>
2023-05-17 13:21:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ response = requests.post(url, data=data)
 if response.status_code == 200:
     st.write('Solicitud POST exitosa')
     st.write('Contenido de la respuesta:')
-    st.write(response.text)
+    st.write(response)
 else:
     st.write('Error en la solicitud POST:', response.status_code)
     
