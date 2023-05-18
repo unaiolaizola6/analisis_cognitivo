75295e6b0babf1fecc9083d38ddbd4b4bf9b60c9
unaiolaizola6 <113601251+unaiolaizola6@users.noreply.github.com>
2023-05-18 09:25:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ data = {
 
 response = requests.post(url, data=data)
 
-st.write(response.status_code)
+st.write(response.json().get('nombre'))
     
 #LEER Y CLASIFICAR LAS RESPUESTAS
 data = pd.read_csv(r'objeto_si.csv')
